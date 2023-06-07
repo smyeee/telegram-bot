4143fc5fed1b835c5291ca10944fd46fefd45699
Amirr95 <arrezvaniyan@gmail.com>
2023-06-07 11:02:13 +0330

    changed .gitignore

--- tutorial.py
+++
@@ -1,2 +0,0 @@
-from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler, CallbackContext
