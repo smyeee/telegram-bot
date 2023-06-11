3fb284de12a014560a18d657ff101f6db57e97b9
Amirr95 <arrezvaniyan@gmail.com>
2023-06-11 16:04:50 +0330

    initial test

--- main.py
+++ main.py
@@ -21,9 +21,11 @@
 
 
 import logging
+import json 
+import datetime
 import geopandas as gpd
-from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler, CallbackContext
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup,  ReplyKeyboardMarkup, ReplyKeyboardRemove
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler, CallbackContext, BasePersistence, ConversationHandler
 from telegram.error import BadRequest
 # Enable logging
 logging.basicConfig(
@@ -32,58 +34,222 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
+
+class JSONPersistence(BasePersistence):
+    def __init__(self, filename):
+        self.filename = filename
+
+    def load(self):
+        try:
+            with open(self.filename, 'r') as f:
+                return json.load(f)
+        except FileNotFoundError:
+            return {}
+
+    def dump(self, data):
+        with open(self.filename, 'w') as f:
+            json.dump(data, f)
+
+
+# Constants for ConversationHandler states
+START, ASK_PHONE, ASK_QUESTION_1, ASK_QUESTION_2, ASK_LOCATION = range(5)
+
 TOKEN = "6004713690:AAHz8olZ6Z4qaODXt5fue3CvaF2VQzCQbms"
 PROXY_URL = "http://192.168.10.185:22222"
 
-def start(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text("Send me your location!")
+persistence = JSONPersistence('bot_data.json')
 
-def handle_location(update: Update, context: CallbackContext) -> None:
-    user_location = update.message.location
-    context.user_data["location"] = user_location
+def start(update: Update, context: CallbackContext):
+    user = update.effective_user
+    user_data = context.user_data
 
-    question = "Choose an option:"
-    options = [("Option 1", "o1"), ("Option 2", "o2"), ("Option 3", "o3")]
-    keyboard = [[InlineKeyboardButton(text, callback_data=data)] for text, data in options]
+    # Check if the user has already signed up
+    if 'phone' in user_data:
+        reply_text = "You have already signed up. Thank you!"
+        update.message.reply_text(reply_text)
+        return ConversationHandler.END
 
-    reply_markup = InlineKeyboardMarkup(keyboard)
-    update.message.reply_text(question, reply_markup=reply_markup)
+    reply_text = f"Hello, {user.first_name}! Please provide your ID, phone number, and answer the following questions."
 
-def handle_button_click(update: Update, context: CallbackContext) -> None:
-    query = update.callback_query
-    query.answer()
+    # Ask for user ID
+    update.message.reply_text("Please enter your ID:")
+    return ASK_PHONE
 
-    choice = query.data
-    location = context.user_data["location"]
-    geo_data = gpd.read_file("test.geojson")
 
-    # Process GeoJSON data and user's choice
-    result = process_geo_data(geo_data, location, choice)
-    query.edit_message_text(f"Your choice: {choice}\nResult: {result}")
+# Function to handle user ID input
+def ask_phone(update: Update, context: CallbackContext):
+    user_data = context.user_data
 
-def process_geo_data(geo_data, location, choice):
-    # Implement your custom logic to extract values from the GeoJSON file
-    # based on the user's location and choice.
+    # Get the user ID
+    user_id = update.message.text.strip()
+    user_data['id'] = user_id
 
-    # Example: Find the nearest point to the user's location
-    user_point = gpd.points_from_xy([location.longitude], [location.latitude])
-    nearest = geo_data.distance(user_point[0]).idxmin()
-    result = geo_data.iloc[nearest]
+    # Ask for phone number
+    update.message.reply_text("Please enter your phone number:")
+    return ASK_QUESTION_1
 
-    return result
 
-def main() -> None:
-    try:
-        updater = Updater(TOKEN, request_kwargs={'proxy_url': PROXY_URL})
-        dispatcher = updater.dispatcher
+# Function to handle phone number input
+def ask_question_1(update: Update, context: CallbackContext):
+    user_data = context.user_data
 
-        dispatcher.add_handler(CommandHandler("start", start))
-        dispatcher.add_handler(MessageHandler(Filters.location, handle_location))
-        dispatcher.add_handler(CallbackQueryHandler(handle_button_click))
+    # Get the phone number
+    phone_number = update.message.text.strip()
+    user_data['phone'] = phone_number
 
-        updater.start_polling()
-        updater.idle()
-    except BadRequest:
-        print('err')
-if __name__ == "__main__":
+    # Ask the first question
+    update.message.reply_text("Question 1: What is your favorite color?", reply_markup=get_color_keyboard())
+    return ASK_QUESTION_2
+
+# Function to handle the first question answer
+def ask_question_2(update: Update, context: CallbackContext):
+    user_data = context.user_data
+
+    # Get the answer to the first question
+    answer_1 = update.message.text.strip()
+    user_data['answer_1'] = answer_1
+
+    # Ask the second question
+    update.message.reply_text("Question 2: What is your favorite animal?", reply_markup=get_animal_keyboard())
+    return ASK_LOCATION
+
+
+# Function to handle the second question answer
+def ask_location(update: Update, context: CallbackContext):
+    user_data = context.user_data
+
+    # Get the answer to the second question
+    answer_2 = update.message.text.strip()
+    user_data['answer_2'] = answer_2
+
+    # Ask for the user's location
+    update.message.reply_text("Please share your location:", reply_markup=ReplyKeyboardRemove())
+    return ConversationHandler.END
+
+# Function to handle the user's location input
+def handle_location(update: Update, context: CallbackContext):
+    user_data = context.user_data
+
+    # Get the user's location
+    location = update.message.location
+    user_data['location'] = {
+        'latitude': location.latitude,
+        'longitude': location.longitude
+    }
+
+    # Save user data
+    context.persistence.update_user_data(update.effective_user.id, user_data)
+
+    update.message.reply_text("Thank you for providing your information! You have successfully signed up.")
+
+
+# Function to get the multi-choice keyboard for question 1
+def get_color_keyboard():
+    keyboard = [['Red', 'Blue', 'Green'], ['Yellow', 'Orange', 'Purple']]
+    return ReplyKeyboardMarkup(keyboard, one_time_keyboard=True)
+
+# Function to get the multi-choice keyboard for question 2
+def get_animal_keyboard():
+    keyboard = [['Dog', 'Cat', 'Bird'], ['Elephant', 'Tiger', 'Lion']]
+    return ReplyKeyboardMarkup(keyboard, one_time_keyboard=True)
+
+# Function to send scheduled messages
+def send_scheduled_messages(context: CallbackContext):
+    # Retrieve all user data
+    user_data = context.persistence.get_user_data()
+    
+    # Loop through all users
+    for user_id, data in user_data.items():
+        user = context.bot.get_chat(user_id)
+        if 'phone' in data:
+            # Send scheduled message to each user
+            message = f"Hello {user.first_name}! This is a scheduled message from the bot."
+            context.bot.send_message(user_id, message)
+
+def main():
+    # Create an instance of Updater and pass the bot token and persistence
+    # updater = Updater(TOKEN, persistence=persistence, use_context=True)
+    updater = Updater(TOKEN, persistence=persistence, use_context=True, request_kwargs={'proxy_url': PROXY_URL})
+
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
+
+    # Add handlers to the dispatcher
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('start', start)],
+        states={
+            ASK_PHONE: [MessageHandler(Filters.text, ask_phone)],
+            ASK_QUESTION_1: [MessageHandler(Filters.text, ask_question_1)],
+            ASK_QUESTION_2: [MessageHandler(Filters.text, ask_question_2)],
+            ASK_LOCATION: [MessageHandler(Filters.location, handle_location)]
+        },
+        fallbacks=[CommandHandler('cancel', start)]
+    )
+
+    dp.add_handler(conv_handler)
+
+    # Start the bot
+    updater.start_polling()
+
+    # Schedule periodic messages
+    job_queue = updater.job_queue
+    job_queue.run_repeating(send_scheduled_messages, interval=datetime.timedelta(days=1).total_seconds())
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT, SIGTERM, or SIGABRT
+    updater.idle()
+
+if __name__ == '__main__':
     main()
+
+# def start(update: Update, context: CallbackContext) -> None:
+#     update.message.reply_text("Send me your location!")
+
+# def handle_location(update: Update, context: CallbackContext) -> None:
+#     user_location = update.message.location
+#     context.user_data["location"] = user_location
+
+#     question = "Choose an option:"
+#     options = [("Option 1", "o1"), ("Option 2", "o2"), ("Option 3", "o3")]
+#     keyboard = [[InlineKeyboardButton(text, callback_data=data)] for text, data in options]
+
+#     reply_markup = InlineKeyboardMarkup(keyboard)
+#     update.message.reply_text(question, reply_markup=reply_markup)
+
+# def handle_button_click(update: Update, context: CallbackContext) -> None:
+#     query = update.callback_query
+#     query.answer()
+
+#     choice = query.data
+#     location = context.user_data["location"]
+#     geo_data = gpd.read_file("test.geojson")
+
+#     # Process GeoJSON data and user's choice
+#     result = process_geo_data(geo_data, location, choice)
+#     query.edit_message_text(f"Your choice: {choice}\nResult: {result}")
+
+# def process_geo_data(geo_data, location, choice):
+#     # Implement your custom logic to extract values from the GeoJSON file
+#     # based on the user's location and choice.
+
+#     # Example: Find the nearest point to the user's location
+#     user_point = gpd.points_from_xy([location.longitude], [location.latitude])
+#     nearest = geo_data.distance(user_point[0]).idxmin()
+#     result = geo_data.iloc[nearest]
+
+#     return result
+
+# def main() -> None:
+#     try:
+#         updater = Updater(TOKEN, request_kwargs={'proxy_url': PROXY_URL})
+#         dispatcher = updater.dispatcher
+
+#         dispatcher.add_handler(CommandHandler("start", start))
+#         dispatcher.add_handler(MessageHandler(Filters.location, handle_location))
+#         dispatcher.add_handler(CallbackQueryHandler(handle_button_click))
+
+#         updater.start_polling()
+#         updater.idle()
+#     except BadRequest:
+#         print('err')
+# if __name__ == "__main__":
+#     main()
