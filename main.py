a77c53ae1779eed435d3b6d34b8a548a4676651c
Amirr95 <arrezvaniyan@gmail.com>
2023-06-14 09:46:51 +0330

    added PicklePersistence

--- main.py
+++ main.py
@@ -1,32 +1,12 @@
-# import logging
-# from telegram import Update
-# from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
-
-# logging.basicConfig(
-#     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-#     level=logging.INFO
-# )
-
-# async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-#     await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
-
-# if __name__ == '__main__':
-#     proxy_url = 'http://192.168.10.185:22222'
-#     application = ApplicationBuilder().token("6004713690:AAHz8olZ6Z4qaODXt5fue3CvaF2VQzCQbms").proxy_url(proxy_url).get_updates_proxy_url(proxy_url).build()
-    
-#     start_handler = CommandHandler('start', start)
-#     application.add_handler(start_handler)
-    
-#     application.run_polling()
-
-
 import logging
-import json 
+import json
 import datetime
 import geopandas as gpd
-from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup,  ReplyKeyboardMarkup, ReplyKeyboardRemove
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler, CallbackContext, BasePersistence, ConversationHandler, PicklePersistence
+from telegram import Bot, Update, InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup, ReplyKeyboardRemove
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler, CallbackContext, \
+    BasePersistence, ConversationHandler, PicklePersistence
 from telegram.error import BadRequest
+
 # Enable logging
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -34,66 +14,22 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
-
-# class JSONPersistence(BasePersistence):
-#     def __init__(self, filename):
-#         self.filename = filename
-#         self.data = self.load()
-
-#     def load(self):
-#         try:
-#             with open(self.filename, 'r') as f:
-#                 return json.load(f)
-#         except FileNotFoundError:
-#             return {}
-
-#     def dump(self):
-#         with open(self.filename, 'w') as f:
-#             json.dump(self.data, f)
-
-#     def get_user_data(self):
-#         return self.data.setdefault('user_data', {})
-
-#     def update_user_data(self, user_id, data):
-#         self.data.setdefault('user_data', {})[str(user_id)] = data
-#         self.dump()
-
-#     def get_bot_data():
-#         pass
-
-#     def update_bot_data():
-#         pass
-
-#     def get_chat_data():
-#         pass
-
-#     def update_chat_data():
-#         pass
-
-#     def get_conversations():
-#         pass
-
-#     def update_conversation():
-#         pass
-
-#     # Implement other required methods for chat and conversation data if needed
-
-
-# persistence = JSONPersistence('bot_data.json')
-
 # Constants for ConversationHandler states
-START, ASK_PHONE, ASK_QUESTION_1, ASK_QUESTION_2, ASK_LOCATION = range(5)
+START, ASK_PHONE, ASK_QUESTION_1, ASK_QUESTION_2, ASK_LOCATION, HANDLE_LOCATION = range(6)
 
 TOKEN = "6004713690:AAHz8olZ6Z4qaODXt5fue3CvaF2VQzCQbms"
 PROXY_URL = "http://192.168.10.185:22222"
 
 persistence = PicklePersistence(filename='bot_data.pickle')
+
+
 def start(update: Update, context: CallbackContext):
     user = update.effective_user
+    # update.message.reply_text(f"id: {user.id}, username: {user.username}")
     user_data = context.user_data
-
+    # update.message.reply_text(user_data)
     # Check if the user has already signed up
-    if 'phone' in user_data:
+    if user.id in persistence.user_data:
         reply_text = "You have already signed up. Thank you!"
         update.message.reply_text(reply_text)
         return ConversationHandler.END
@@ -130,6 +66,7 @@ def ask_question_1(update: Update, context: CallbackContext):
     update.message.reply_text("Question 1: What is your favorite color?", reply_markup=get_color_keyboard())
     return ASK_QUESTION_2
 
+
 # Function to handle the first question answer
 def ask_question_2(update: Update, context: CallbackContext):
     user_data = context.user_data
@@ -153,11 +90,13 @@ def ask_location(update: Update, context: CallbackContext):
 
     # Ask for the user's location
     update.message.reply_text("Please share your location:", reply_markup=ReplyKeyboardRemove())
-    return ConversationHandler.END
+    return HANDLE_LOCATION
 
-# Function to handle the user's location input
+
+# Function to handle the user's location input  persistence: BasePersistence, bot: Bot
 def handle_location(update: Update, context: CallbackContext):
     user_data = context.user_data
+    # update.message.reply_text("Please share your location!")
 
     # Get the user's location
     location = update.message.location
@@ -165,11 +104,15 @@ def handle_location(update: Update, context: CallbackContext):
         'latitude': location.latitude,
         'longitude': location.longitude
     }
-
     # Save user data
-    context.persistence.update_user_data(update.effective_user.id, user_data)
-
+    # logger.log(msg=context.user_data)
+    print("bot_data", context.bot_data)
+    print("user_data", context.user_data)
+    # persistence("bot")
+    # context.persistence.update_user_data(update.effective_user.id, user_data)
+    persistence.update_user_data(user_id=update.effective_user.id, data = user_data)
     update.message.reply_text("Thank you for providing your information! You have successfully signed up.")
+    return ConversationHandler.END
 
 
 # Function to get the multi-choice keyboard for question 1
@@ -177,28 +120,50 @@ def get_color_keyboard():
     keyboard = [['Red', 'Blue', 'Green'], ['Yellow', 'Orange', 'Purple']]
     return ReplyKeyboardMarkup(keyboard, one_time_keyboard=True)
 
+
 # Function to get the multi-choice keyboard for question 2
 def get_animal_keyboard():
     keyboard = [['Dog', 'Cat', 'Bird'], ['Elephant', 'Tiger', 'Lion']]
     return ReplyKeyboardMarkup(keyboard, one_time_keyboard=True)
 
+
 # Function to send scheduled messages
-def send_scheduled_messages(context: CallbackContext):
+# def send_scheduled_messages(context: CallbackContext):
+#     # Retrieve all user data
+#     user_data = context.persistence.get_user_data()
+
+#     # Loop through all users
+#     for user_id, data in user_data.items():
+#         user = context.bot.get_chat(user_id)
+#         if 'phone' in data:
+#             # Send scheduled message to each user
+#             message = f"Hello {user.first_name}! This is a scheduled message from the bot."
+#             context.bot.send_message(user_id, message)
+
+# Function to send personalized scheduled messages
+def send_scheduled_messages(persistence: persistence, bot: Bot):
     # Retrieve all user data
-    user_data = context.persistence.get_user_data()
-    
+    user_data = persistence.get_user_data()
+
     # Loop through all users
     for user_id, data in user_data.items():
-        user = context.bot.get_chat(user_id)
+        user = bot.get_chat(user_id)
         if 'phone' in data:
-            # Send scheduled message to each user
-            message = f"Hello {user.first_name}! This is a scheduled message from the bot."
-            context.bot.send_message(user_id, message)
+            # Customize the message based on user's data
+            message = f"Hello {user.first_name}! This is a scheduled message from the bot.\n"
+            message += f"Your phone number: {data['phone']}\n"
+            message += f"Answer to Question 1: {data['answer_1']}\n"
+            message += f"Answer to Question 2: {data['answer_2']}\n"
+            # ... add more personalized information
+            # message = "Hello..."
+            # message = data
+            bot.send_message(user_id, message)
+
 
 def main():
     # Create an instance of Updater and pass the bot token and persistence
     # updater = Updater(TOKEN, persistence=persistence, use_context=True)
-    updater = Updater(TOKEN, persistence=persistence, use_context=True) #, request_kwargs={'proxy_url': PROXY_URL})
+    updater = Updater(TOKEN, persistence=persistence, use_context=True)  # , request_kwargs={'proxy_url': PROXY_URL})
 
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
@@ -210,7 +175,8 @@ def main():
             ASK_PHONE: [MessageHandler(Filters.text, ask_phone)],
             ASK_QUESTION_1: [MessageHandler(Filters.text, ask_question_1)],
             ASK_QUESTION_2: [MessageHandler(Filters.text, ask_question_2)],
-            ASK_LOCATION: [MessageHandler(Filters.location, handle_location)]
+            ASK_LOCATION: [MessageHandler(Filters.text, ask_location)],
+            HANDLE_LOCATION: [MessageHandler(Filters.location, handle_location)]
         },
         fallbacks=[CommandHandler('cancel', start)]
     )
@@ -222,10 +188,12 @@ def main():
 
     # Schedule periodic messages
     job_queue = updater.job_queue
-    job_queue.run_repeating(send_scheduled_messages, interval=datetime.timedelta(seconds=30).total_seconds())
+    job_queue.run_repeating(lambda context: send_scheduled_messages(persistence, context.bot),
+                            interval=datetime.timedelta(seconds=60).total_seconds())
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT, SIGTERM, or SIGABRT
     updater.idle()
+
 
 if __name__ == '__main__':
     main()
