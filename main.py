f99095ac06e22cf945338718aee68b67d7d42f3f
Amirr95 <arrezvaniyan@gmail.com>
2023-07-17 16:40:11 +0330

    fixed job times

--- main.py
+++ main.py
@@ -726,13 +726,13 @@ def main():
         job_queue.run_repeating(lambda context: get_member_count(context.bot), interval=7200, first=60)
         job_queue.run_repeating(lambda context: send_todays_weather(context.bot),
                                 interval=datetime.timedelta(days=1),
-                                first=datetime.timedelta(seconds=43200))
+                                first=datetime.time(5, 30))
         job_queue.run_repeating(lambda context: send_tomorrows_weather(context.bot),
                                 interval=datetime.timedelta(days=1),
-                                first=datetime.timedelta(seconds=43250))
+                                first=datetime.time(5, 31))
         job_queue.run_repeating(lambda context: send_advice_to_users(context.bot),
                                 interval=datetime.timedelta(days=1),
-                                first=datetime.timedelta(seconds=43300))
+                                first=datetime.time(5, 32))
         job_queue.run_once(lambda context: send_up_notice(context.bot), when=5)
         # Run the bot until you press Ctrl-C or the process receives SIGINT, SIGTERM, or SIGABRT
         updater.idle()
